ebca5b32d0ae521379002d7eb0546e04c0230aee
Gallner131 <72497136+Gallner131@users.noreply.github.com>
2025-10-11 15:42:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ app.use(passport.initialize());
 app.use(authenticateUser);
 
 // Routes
+app.use("/api/webhook", require("./routes/webhook"));
 app.use("/api", routes);
 
 app.get("/health", (req, res) => {
