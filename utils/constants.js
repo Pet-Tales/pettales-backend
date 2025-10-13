8b1bab9509b19bac354817b32bdf2bc36a093ef8
Gallner131 <72497136+Gallner131@users.noreply.github.com>
2025-10-13 11:27:29 +0100

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -77,21 +77,17 @@ const LULU_WEBHOOK_SECRET = process.env.LULU_WEBHOOK_SECRET;
 const LULU_WEBHOOK_URL = process.env.LULU_WEBHOOK_URL;
 const LULU_BASE64_ENCODED_KEY_SECRET = process.env.LULU_BASE64_ENCODED_KEY_SECRET;
 
-// Illustration Styles
+// Illustration Styles (force prod URLs; do NOT read from env)
 const ILLUST_ANIME =
-  process.env.ILLUST_ANIME ||
   "https://storage.pettales.ai/_static/illustration_styles/illust_anime_9.jpg";
 
 const ILLUST_DISNEY =
-  process.env.ILLUST_DISNEY ||
   "https://storage.pettales.ai/_static/illustration_styles/illust_disney_9.jpg";
 
 const ILLUST_VECTOR_ART =
-  process.env.ILLUST_VECTOR_ART ||
   "https://storage.pettales.ai/_static/illustration_styles/illust_vector_art_9.jpg";
 
 const ILLUST_CLASSIC_WATERCOLOR =
-  process.env.ILLUST_CLASSIC_WATERCOLOR ||
   "https://storage.pettales.ai/_static/illustration_styles/illust_classic_watercolor_1.jpg";
 
 
